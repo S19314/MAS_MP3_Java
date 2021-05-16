5525be6e3e3b92c27c0d9c5ffe900340a0a17ed4
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-05-16 21:27:31 +0300

    Factory, added: gets/sets, constructor, manage fields.  Version 1.1

--- src/mp3/Factory.java
+++ src/mp3/Factory.java
@@ -14,12 +14,64 @@ public class Factory {
     /*
         Fields of Bakery
     */
-    private String breadType;
-            
+    private String[] breadTypes; // For Producing
+    
     /*
         Fields of Beverage fabricator
     */
+    private int maxProduceLitresDrinkPerDay;
     
     private EnumSet<FactoryType> factoryKind = EnumSet.of(FactoryType.Factory);
+
+    /*
+        Creates Bakery-Beverage fabricator.
+    */
+    public Factory(Date creationDate, String name, String[] breadTypes, int maxProduceLitresDrinkPerDay) {
+        this.creationDate = creationDate;
+        this.name = name;
+        this.breadTypes = breadTypes;
+        this.maxProduceLitresDrinkPerDay = maxProduceLitresDrinkPerDay;
+    }
+
+    public int getMaxProduceLitresDrinkPerDay() {
+        return maxProduceLitresDrinkPerDay;
+    }
+
+    public void setMaxProduceLitresDrinkPerDay(int maxProduceLitresDrinkPerDay) {
+        this.maxProduceLitresDrinkPerDay = maxProduceLitresDrinkPerDay;
+    }
+    
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getBreadTypes() {
+        return breadTypes;
+    }
+
+    public void setBreadTypes(String[] breadTypes) {
+        this.breadTypes = breadTypes;
+    }
+
+    public EnumSet<FactoryType> getFactoryKind() {
+        return factoryKind;
+    }
+
+    public void setFactoryKind(EnumSet<FactoryType> factoryKind) {
+        this.factoryKind = factoryKind;
+    }
+    
     
 }
